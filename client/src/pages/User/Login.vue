1bc69fddb07ab08acb730e45f6c85264705bc0fb
Luca Di Gerlando <lucadigerlando@gmail.com>
2018-10-01 21:05:38 +0200

    Create owner in local storage

--- client/src/pages/User/Login.vue
+++ client/src/pages/User/Login.vue
@@ -1,39 +1,39 @@
 <template>
   <div class="row mt-5 mb-5">
     <div class="col-lg-12 col-md-12 col-sm-12">
-    <div class="inner cover">
-      <h1 class="text-center mb-5">User Login</h1>
-      <button type="button" class="btn btn-primary btn-block" @click="openFbLoginDialog">Facebook Login</button>
-      <div class="text-center mt-4 mb-5" style="width: 10rem; height: 13px; border-bottom: 1px dotted #ccc; margin: 0 auto;">
-        <span style="font-size: 16px; background-color: #FFF; padding: 0 10px; font-weight: bold; color: #999">
-          OR
-        </span>
-      </div>
-      <form>
-        <div class="form-group">
+      <div class="inner cover">
+        <h1 class="text-center mb-5">User Login</h1>
+        <button type="button" class="btn btn-primary btn-block" @click="openFbLoginDialog">Facebook Login</button>
+        <div class="text-center mt-4 mb-5" style="width: 10rem; height: 13px; border-bottom: 1px dotted #ccc; margin: 0 auto;">
+          <span style="font-size: 16px; background-color: #FFF; padding: 0 10px; font-weight: bold; color: #999">
+            OR
+          </span>
+        </div>
+        <form>
+          <div class="form-group">
             <input class="form-control" type="text" aria-describedby="emailHelp" v-model="form.username" placeholder="Username">
-        </div>
-        <div class="form-group">
+          </div>
+          <div class="form-group">
             <input class="form-control" type="password" v-model="form.password" placeholder="password">
-        </div>
-        <div class="form-check">
-          <label class="form-check-label">
-            <input type="checkbox" class="form-check-input">
-            Remember me
-          </label>
-        </div>
-        <br>
-        <button type="submit" class="btn btn-warning btn-block" @click="login">Log in</button>
-      </form>
-      <router-link to="/">
-        Torna alla home
-      </router-link>
-      <router-link to="/register">
-        Rigistrazione
-      </router-link>
+          </div>
+          <div class="form-check">
+            <label class="form-check-label">
+              <input type="checkbox" class="form-check-input">
+              Remember me
+            </label>
+          </div>
+          <br>
+          <button type="submit" class="btn btn-warning btn-block" @click="login">Log in</button>
+        </form>
+        <router-link to="/">
+          Torna alla home
+        </router-link>
+        <router-link to="/register">
+          Rigistrazione
+        </router-link>
+      </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
@@ -52,94 +52,102 @@ export default {
   },
   methods: {
     openFbLoginDialog () {
-       FB.login(this.checkLoginState, { scope: 'email' })
+      FB.login(this.checkLoginState, { scope: 'email' })
     },
     checkLoginState (response) {
       if (response.status === 'connected') {
         console.log(response.authResponse.accessToken);
-
         //this.$router.push('/');
         //sessionStorage.setItem('token',response.authResponse.accessToken)
         //this.$store.dispatch('updateToken', response.authResponse.accessToken)
 
         FB.api('/me', (response) => {
-            console.log(JSON.stringify(response))
-            sessionStorage.setItem('user',JSON.stringify(response))
+
+          console.log(JSON.stringify(response))
+
+          const owner = {
+            "id": response.id,
+            "social": "facebook",
+            "token": response.authResponse.accessToken
+          }
+
+          sessionStorage.setItem('owner',JSON.stringify(owner))
+          
         });
         /*
         FB.api( "/me/feed",
-                "POST",
-                {
-                  "message": "Test"
-                },
-                (response) => {
-                  console.log(JSON.stringify(response));
-                }
-        )
-        */
-      } else if (response.status === 'not_authorized') {
-        // the user is logged in to Facebook,
-        // but has not authenticated your app
-      } else {
-        // the user isn't logged in to Facebook.
-      }
-    },
-    login (evt) {
-      evt.preventDefault()
-      // this.$router.push('/home')
-      API.post(`user/login`, this.form)
-      .then(response => {
-        // JSON responses are automatically parsed.
-
-        this.$router.push('/');
-        const decoded = jwt_decode(response.data.token)
-        const role = decoded.role
-        sessionStorage.setItem('token',response.data.token);
-        this.$store.dispatch('updateToken', response.data.token)
-
-        // this.$store.dispatch('setRole', role)
-        // this.$acl.change(role)
-      })
-      .catch(e => {
-        console.log(e)
-      })
+        "POST",
+        {
+        "message": "Test"
+      },
+      (response) => {
+      console.log(JSON.stringify(response));
     }
-  }
+  )
+  */
+} else if (response.status === 'not_authorized') {
+  // the user is logged in to Facebook,
+  // but has not authenticated your app
+} else {
+  // the user isn't logged in to Facebook.
+}
+},
+login (evt) {
+  evt.preventDefault()
+  // this.$router.push('/home')
+  API.post(`user/login`, this.form)
+  .then(response => {
+    // JSON responses are automatically parsed.
+
+    this.$router.push('/');
+    const decoded = jwt_decode(response.data.token)
+    const role = decoded.role
+    sessionStorage.setItem('token',response.data.token);
+    this.$store.dispatch('updateToken', response.data.token)
+
+    // this.$store.dispatch('setRole', role)
+    // this.$acl.change(role)
+  })
+  .catch(e => {
+    console.log(e)
+  })
+}
+}
 }
 </script>
 
 <style scoped>
 .inner {
-     padding: 2rem;
+  padding: 2rem;
 }
 
 .inner.cover {
-     border: 1px solid #f0f0f0;
-     padding: 2.5rem;
+  border: 1px solid #f0f0f0;
+  padding: 2.5rem;
 }
 
 .cover {
-     padding: 0 1.5rem;
+  padding: 0 1.5rem;
 }
 
 .cover .btn-lg {
-     padding: .75rem 1.25rem;
-     font-weight: bold;
+  padding: .75rem 1.25rem;
+  font-weight: bold;
 }
 
 @media (min-width: 40em) {
 
-     .cover-container {
-        width: 100%;
-     }
+  .cover-container {
+    width: 100%;
+  }
 
 }
 
 @media (min-width: 62em) {
 
-     .cover-container {
-        width: 42rem;
-     }
+  .cover-container {
+    width: 42rem;
+  }
 
 }
 
