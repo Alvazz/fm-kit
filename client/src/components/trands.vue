c07d3396f9b6745710aa44a870361fc2cccff0a0
Luca Di Gerlando <lucadigerlando@gmail.com>
2018-10-05 23:07:38 +0200

    commit

--- client/src/components/trands.vue
+++ client/src/components/trands.vue
@@ -2,14 +2,8 @@
   <div class="card">
     <div class="trands">
       <ul>
-        <li>
-          <span class="count">1</span> Test
-        </li>
-        <li>
-          <span class="count">2</span> Test
-        </li>
-        <li>
-          <span class="count">2</span> Test
+        <li v-for="i in 5">
+          <span class="count">{{i}}</span> Lorem ipsum dolor sit amet, consectetur adipisicing elit.
         </li>
       </ul>
     </div>
@@ -28,16 +22,23 @@ export default {
 }
 .trands ul {
   list-style: none;
-  padding: 0
+  padding: 0;
+  font-size: .9rem;
 }
 .trands ul li{
   min-height: 70px;
   border-bottom: 1px solid #c2c2c2;
   margin: 0 10px;
+  padding: 14px 0;
   display: flex;
   align-items: center;
 }
+.trands ul li:last-child{
+  border-bottom: 0px solid #c2c2c2;
+  padding-bottom: 0;
+}
 .trands ul li .count{
+  color: #c2c2c2;
   font-size: 1rem;
   font-weight: bold;
   padding-right: 10px;
